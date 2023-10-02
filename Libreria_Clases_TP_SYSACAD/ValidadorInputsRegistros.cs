c329f9b872f3840a15829e0a3aa3f1e05aedf375
MarianoAquino6 <mariano.aquino@hotmail.com.ar>
2023-10-02 19:20:58 -0300

    version 2.10 v3

--- Libreria_Clases_TP_SYSACAD/ValidadorInputsRegistros.cs
+++ Libreria_Clases_TP_SYSACAD/ValidadorInputsRegistros.cs
@@ -15,59 +15,49 @@ namespace Libreria_Clases_TP_SYSACAD
 
             string mensajeADevolver = "";
 
-            if (Validador.ValidarCampos(listaDeCamposIngresados))
+            // Validar el nombre 
+            if (!Regex.IsMatch(listaDeCamposIngresados[0], @"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$"))
             {
+                listaErrores.AppendLine("Nombre");
+            }
 
-                // Validar el nombre 
-                if (!Regex.IsMatch(listaDeCamposIngresados[0], @"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$"))
-                {
-                    listaErrores.AppendLine("Nombre");
-                }
+            // Validar el Legajo
+            if (!Regex.IsMatch(listaDeCamposIngresados[1], @"^\d{8}$"))
+            {
+                listaErrores.AppendLine("Legajo");
+            }
 
-                // Validar el Legajo
-                if (!Regex.IsMatch(listaDeCamposIngresados[1], @"^\d{8}$"))
-                {
-                    listaErrores.AppendLine("Legajo");
-                }
+            //Validar Direccion
+            if (!Regex.IsMatch(listaDeCamposIngresados[2], @"^[a-zA-Z0-9\s]+$"))
+            {
+                listaErrores.AppendLine("Dirección");
+            }
 
-                //Validar Direccion
-                if (!Regex.IsMatch(listaDeCamposIngresados[2], @"^[a-zA-Z0-9\s]+$"))
-                {
-                    listaErrores.AppendLine("Dirección");
-                }
+            // Validar telefono
+            if (!Regex.IsMatch(listaDeCamposIngresados[3], @"^\d{8}(\d{2})?$"))
+            {
+                listaErrores.AppendLine("Telefono");
+            }
 
-                // Validar telefono
-                if (!Regex.IsMatch(listaDeCamposIngresados[3], @"^\d{8}(\d{2})?$"))
-                {
-                    listaErrores.AppendLine("Telefono");
-                }
+            // Validar correo 
+            if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+            {
+                listaErrores.AppendLine("Correo");
+            }
 
-                // Validar correo 
-                if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
-                {
-                    listaErrores.AppendLine("Correo");
-                }
-
-                //Verificar errores y devolverlos como mensajes
-                if (listaErrores.Length > 1)
-                {
-                    listaErrores.AppendLine("Revisar!!!");
-                    mensajeADevolver = listaErrores.ToString();
-                }
-                else
-                {
-                    listaErrores.Clear();
-                    mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
-                }
+            //Verificar errores y devolverlos como mensajes
+            if (listaErrores.Length > 1)
+            {
+                listaErrores.AppendLine("Revisar!!!");
+                mensajeADevolver = listaErrores.ToString();
             }
             else
             {
                 listaErrores.Clear();
-                mensajeADevolver = listaErrores.Append("CAMPOS VACIOS").ToString(); ;
+                mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
             }
 
             return mensajeADevolver;
-
         }
 
         public static string ValidarDatosCurso(List<string> listaDeCamposIngresados)
@@ -76,59 +66,49 @@ namespace Libreria_Clases_TP_SYSACAD
 
             string mensajeADevolver = "";
 
-            if (Validador.ValidarCampos(listaDeCamposIngresados))
+            // Validar el nombre de la MATERIA 
+            if (!Regex.IsMatch(listaDeCamposIngresados[0], @"^[a-zA-Z0-9\s]+$"))
             {
+                listaErrores.AppendLine("Nombre de la materia");
+            }
 
-                // Validar el nombre de la MATERIA 
-                if (!Regex.IsMatch(listaDeCamposIngresados[0], @"^[a-zA-Z0-9\s]+$"))
-                {
-                    listaErrores.AppendLine("Nombre de la materia");
-                }
+            // Validar el CODIGO
+            if (!Regex.IsMatch(listaDeCamposIngresados[1], @"^[a-zA-Z0-9]+$"))
+            {
+                listaErrores.AppendLine("Codigo");
+            }
 
-                // Validar el CODIGO
-                if (!Regex.IsMatch(listaDeCamposIngresados[1], @"^[a-zA-Z0-9]+$"))
-                {
-                    listaErrores.AppendLine("Codigo");
-                }
+            // Validar el numero de aula
+            if (!Regex.IsMatch(listaDeCamposIngresados[2], @"^\d{3}$"))
+            {
+                listaErrores.AppendLine("Número de Aula");
+            }
 
-                // Validar el numero de aula
-                if (!Regex.IsMatch(listaDeCamposIngresados[2], @"^\d{3}$"))
-                {
-                    listaErrores.AppendLine("Número de Aula");
-                }
+            // Validar Descripcion
+            if (!Regex.IsMatch(listaDeCamposIngresados[3], @"^[a-zA-Z0-9ñÑ°\s.,-]+$"))
+            {
+                listaErrores.AppendLine("Descripcion");
+            }
 
-                // Validar Descripcion
-                if (!Regex.IsMatch(listaDeCamposIngresados[3], @"^[a-zA-Z0-9ñÑ°\s.,-]+$"))
-                {
-                    listaErrores.AppendLine("Descripcion");
-                }
+            // Validar Cupo maximo 
+            if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[0-9]+$"))
+            {
+                listaErrores.AppendLine("Cupo maximo");
+            }
 
-                // Validar Cupo maximo 
-                if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[0-9]+$"))
-                {
-                    listaErrores.AppendLine("Cupo maximo");
-                }
-
-                //Verificar errores y devolverlos como mensajes
-                if (listaErrores.Length > 1)
-                {
-                    listaErrores.AppendLine("Revisar!!!");
-                    mensajeADevolver = listaErrores.ToString();
-                }
-                else
-                {
-                    listaErrores.Clear();
-                    mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
-                }
+            //Verificar errores y devolverlos como mensajes
+            if (listaErrores.Length > 1)
+            {
+                listaErrores.AppendLine("Revisar!!!");
+                mensajeADevolver = listaErrores.ToString();
             }
             else
             {
                 listaErrores.Clear();
-                mensajeADevolver = listaErrores.Append("CAMPOS VACIOS").ToString(); ;
+                mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
             }
 
             return mensajeADevolver;
-
         }
 
         public static string ValidarNuevoAdmin(List<string> listaDeCamposIngresados)
@@ -137,30 +117,22 @@ namespace Libreria_Clases_TP_SYSACAD
 
             string mensajeADevolver = "";
 
-            if (Validador.ValidarCampos(listaDeCamposIngresados))
+            // Validar correo 
+            if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
             {
-                // Validar correo 
-                if (!Regex.IsMatch(listaDeCamposIngresados[4], @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
-                {
-                    listaErrores.AppendLine("Correo");
-                }
+                listaErrores.AppendLine("Correo");
+            }
 
-                //Verificar errores y devolverlos como mensajes
-                if (listaErrores.Length > 1)
-                {
-                    listaErrores.AppendLine("Revisar!!!");
-                    mensajeADevolver = listaErrores.ToString();
-                }
-                else
-                {
-                    listaErrores.Clear();
-                    mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
-                }
+            //Verificar errores y devolverlos como mensajes
+            if (listaErrores.Length > 1)
+            {
+                listaErrores.AppendLine("Revisar!!!");
+                mensajeADevolver = listaErrores.ToString();
             }
             else
             {
                 listaErrores.Clear();
-                mensajeADevolver = listaErrores.Append("CAMPOS VACIOS").ToString(); ;
+                mensajeADevolver = listaErrores.Append("ALTA EXITOSA").ToString();
             }
 
             return mensajeADevolver;
